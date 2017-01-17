eb9f19d7a2def4bdd3230c9e248b9b0dd8629d13
Canavaggio Lorenzo <lorenzo.canavaggio@laposte.net>
2017-01-17 20:16:48 +0100

    Update databaseConnectionClass.cs
    Connection class with generic SQL Client

--- databaseConnectionClass.cs
+++ databaseConnectionClass.cs
@@ -1,127 +1,188 @@
-using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 class DataBaseConnect
-
 {
-
-    private MySqlConnection connection;
-
+    private SqlCommand myCommand;
+    private SqlDataReader myReader;
+    private SqlConnection connection;
     private string server;
-
     private string database;
-
     private string uid;
-
     private string password;
-
- 
+    private string connectionString;
 
     //Constructeur
-
     public DataBaseConnect()
-
     {
-
         Initialize();
-
     }
-
-//Initialisation des paramètres de connexion
-
+    //Initialisation des paramètres de connexion
     private void Initialize()
-
-    {
-
+{
         server = "localhost";
-
         database = "mabase"
-
         uid = "username";
-
         password = "mm";
+        connectionString = "Data Source="+server+ ";" +"Initial Catalog="+database+";" +"User id="+uid+";" +"Password="+password+";";   
+        connection = new SqlConnection(connectionString);
+}
 
-        string connectionString;
-
-        connectionString = "SERVER=" + server + ";" + "DATABASE=" +
-
-                       database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
-
- 
-
-        connection = new MySqlConnection(connectionString);
-
+    public string getServer() 
+    {
+        return this.server;
     }
 
- 
+    public void setServer(string newServer)
+    {
+         this.server= newServer;
+    }
 
+    public string getDatabase()
+    {
+        return this.database;
+    }
+
+    public void setDatabase(string newDatabase)
+    {
+        this.database = newDatabase;
+    }
+    public string getUid()
+    {
+        return this.uid;
+    }
+
+    public void setUid(string newUid)
+    {
+        this.uid = newUid;
+    }
+    public string getPassword()
+    {
+        return this.password;
+    }
+
+    public void setPassword(string newPassword)
+    {
+        this.password = newPassword;
+    }
+    
     //open
-
     private bool OpenConnection()
-
     {
+        try
+        {
+            connection.Open();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
 
     }
-
- 
-
     //Close
-
     private bool CloseConnection()
-
     {
 
+        try
+        {
+            connection.Close();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+
     }
-
- 
-
     //Insert
-
     public void Insert(string marequete)
-
     {
-
+        try
+        { 
+            myCommand = new SqlCommand(marequete, connection);
+            OpenConnection();
+            myCommand.ExecuteNonQuery();
+            CloseConnection(); 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
-
- 
-
     //Update
-
     public void Update(string marequete)
-
     {
 
+        try
+        {
+            myCommand = new SqlCommand(marequete, connection);
+            OpenConnection();
+            myCommand.ExecuteNonQuery();
+            CloseConnection();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
-
- 
-
     //Delete
-
     public void Delete(string marequete)
-
     {
-
+        try
+        {
+            myCommand = new SqlCommand(marequete, connection);
+            OpenConnection();
+            myCommand.ExecuteNonQuery();
+            CloseConnection();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
-
- 
-
     //Select
-
-    public string Select(string marequete)
-
+    public DataTable Select(string marequete)
     {
-
-//Retourne une chaine resultat de la requête mais au final on renverra de quoi traiter le résultat de la requête, le type retourné ne sera donc pas une chaîne
-
+        try
+        {
+            myCommand = new SqlCommand(marequete, connection);
+            OpenConnection();
+            SqlDataAdapter MyAdapter = new SqlDataAdapter();
+            MyAdapter.SelectCommand = myCommand;
+            DataTable dataTable = new DataTable();
+            MyAdapter.Fill(dataTable);      
+            CloseConnection();            
+            return dataTable; // on retourne une datatable qu'on pourra assigner à une Datagridview.source
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return null;
+        }
     }
-
- 
-
     //Count
-
     public int Count(string marequete)
-
     {
 
-    }
+        try
+        {
+            myCommand = new SqlCommand(marequete, connection);
+            OpenConnection();
+            return int.Parse(myCommand.ExecuteScalar().ToString());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return 0; // return 0 ?
+        }
 
+    }
 }
