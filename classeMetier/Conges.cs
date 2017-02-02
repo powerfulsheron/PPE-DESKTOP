d4696598f49ced1cd549e58efeda8a66dc20da45
RaphaelDG <Raphael.dulong@gmail.com>
2017-02-02 20:46:27 +0100

    Add files via upload

---
+++ classeMetier/Conges.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE_Belle_Table_Classes_métier
+{
+    class Conges
+    {
+        private int numConge;
+        private DateTime dateDebutConge;
+        private DateTime dateFinConge;
+
+        public Conges()
+        {
+
+        }
+        //Setter
+        public void SetnumConge(int unnumConge)
+        {
+            numConge = unnumConge;
+        }
+
+
+        public void SetdateDebutConge(DateTime unedateDebutConge)
+        {
+            dateDebutConge = unedateDebutConge;
+        }
+
+        public void SetdateFinConge(DateTime unedateFinConge)
+        {
+            dateFinConge = unedateFinConge;
+        }
+
+        //Getter
+        public int GetLenumConge()
+        {
+            return numConge;
+        }
+
+        public string GetdateDebutConge()
+        {
+            return dateDebutConge;
+        }
+
+        public string GetdateFinConge()
+        {
+            return dateFinConge;
+        }
+    }
+}
