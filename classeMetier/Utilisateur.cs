d022884862d87a2cdc5e6255686614c365b8d200
AxelleS <saillyaxelle@hotmail.fr>
2017-02-24 09:16:51 +0100

    Update Utilisateur.cs

--- classeMetier/Utilisateur.cs
+++ classeMetier/Utilisateur.cs
@@ -207,5 +207,12 @@ namespace PPE_Belle_Table_Classes_métier
             DataTable dt = maConnexion.Select("SELECT * FROM MAIL WHERE id_utilisateur=",id_utilisateur);
             //ouéouéoué on sait plus a quoi ça sert
         }
+        
+        public DataTable chargerLePorteFeuille(int idUtilsateur)
+        {
+            DataTable LePortefeuille = DatabaseConnect.Select("SELECT * FROM portefeuille WHERE numUtilisateur=",idUtilsateur);
+
+            return LePortefeuille;
+        }
     }
 }
