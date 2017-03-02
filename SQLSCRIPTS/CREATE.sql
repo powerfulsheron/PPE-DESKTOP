5d4757a2b5c5fac44b4a1f9e4e24666130409a27
Canavaggio Lorenzo <lorenzo.canavaggio@laposte.net>
2017-03-02 16:33:39 +0100

    Update CREATE.sql

--- SQLSCRIPTS/CREATE.sql
+++ SQLSCRIPTS/CREATE.sql
@@ -1,22 +1,30 @@
 #------------------------------------------------------------
-# Table: COMMERCIAL
+#        Script MySQL.
 #------------------------------------------------------------
 
-DROP TABLE COMMERCIAL;
 
-CREATE TABLE COMMERCIAL(
-        id_salarie               int (10) Auto_increment  NOT NULL ,
-        nom_salarie              Varchar (25) NOT NULL ,
-        prenom_salarie           Varchar (25) NOT NULL ,
-        adresse_salarie          Varchar (50) NOT NULL ,
-        cp_salarie               Varchar (10) NOT NULL ,
-        ville_salarie            Varchar (20) NOT NULL ,
-        tel_salarie              Varchar (20) NOT NULL ,
-        mail_salarie             Varchar (30) NOT NULL ,
+#------------------------------------------------------------
+# Table: UTILISATEUR
+#------------------------------------------------------------
+
+CREATE TABLE UTILISATEUR(
+        numUtilisateur           int (11) Auto_increment  NOT NULL ,
+        nomUtilisateur           Varchar (25) NOT NULL ,
+        adresseUtilisateur       Varchar (25) NOT NULL ,
+        cpUtilisateur            Varchar (25) NOT NULL ,
+        villeUtilisateur         Varchar (25) NOT NULL ,
+        telUtilisateur           Varchar (25) NOT NULL ,
+        mailUtilisateur          Varchar (25) NOT NULL ,
         distanceParcourueSemaine Int ,
-        numUtilisateur           Int ,
+        prenomUtilisateur        Varchar (25) NOT NULL ,
+        loginUtilisateur         Varchar (30) ,
+        passwordUtilisateur      Varchar (25) ,
+        dateDernierLogin         Datetime ,
+        nbTentatives             Int ,
+        codeTypeUtilisateur      Int NOT NULL ,
+        idPlanning               Varchar (25) ,
         idPortefeuille           Int ,
-        PRIMARY KEY (id_salarie )
+        PRIMARY KEY (numUtilisateur )
 )ENGINE=InnoDB;
 
 
@@ -24,30 +32,26 @@ CREATE TABLE COMMERCIAL(
 # Table: TYPERDV
 #------------------------------------------------------------
 
-DROP TABLE TYPERDV;
-
 CREATE TABLE TYPERDV(
         id_type_rdv Int NOT NULL ,
-        type_rdv    Varchar (30) NOT NULL ,
+        type_rdv    Varchar (25) NOT NULL ,
         PRIMARY KEY (id_type_rdv )
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: STRUCTURE_INTERLOCUTEUR
+# Table: STRUCTURE
 #------------------------------------------------------------
 
-DROP TABLE STRUCTURE_INTERLOCUTEUR;
-
-CREATE TABLE STRUCTURE_INTERLOCUTEUR(
-        num_structure        int (10) Auto_increment  NOT NULL ,
-        denomination_sociale Varchar (30) NOT NULL ,
-        adresse_structure    Varchar (50) NOT NULL ,
-        cp_structure         Varchar (10) NOT NULL ,
-        ville_structure      Varchar (20) NOT NULL ,
+CREATE TABLE STRUCTURE(
+        num_structure        int (11) Auto_increment  NOT NULL ,
+        denomination_sociale Varchar (25) NOT NULL ,
+        adresse_structure    Varchar (25) NOT NULL ,
+        cp_structure         Varchar (25) NOT NULL ,
+        ville_structure      Varchar (25) NOT NULL ,
         distance_siege       Float NOT NULL ,
-        plan_accees          Varchar (100) NOT NULL ,
-        infos_supplementaire Varchar (100) NOT NULL ,
+        plan_accees          Varchar (25) NOT NULL ,
+        infos_supplementaire Varchar (25) NOT NULL ,
         codeTypeStructure    Int NOT NULL ,
         PRIMARY KEY (num_structure )
 )ENGINE=InnoDB;
@@ -57,14 +61,12 @@ CREATE TABLE STRUCTURE_INTERLOCUTEUR(
 # Table: INTERLOCUTEUR
 #------------------------------------------------------------
 
-DROP TABLE INTERLOCUTEUR;
-
 CREATE TABLE INTERLOCUTEUR(
-        id_interlocuteur     int (10) Auto_increment  NOT NULL ,
+        id_interlocuteur     int (11) Auto_increment  NOT NULL ,
         nom_interlocuteur    Varchar (25) NOT NULL ,
         prenom_interlocuteur Varchar (25) NOT NULL ,
-        tel_interlocuteur    Varchar (20) NOT NULL ,
-        mail_interlocuteur   Varchar (30) NOT NULL ,
+        tel_interlocuteur    Varchar (25) NOT NULL ,
+        mail_interlocuteur   Varchar (25) NOT NULL ,
         num_individu         Int NOT NULL ,
         idPortefeuille       Int ,
         PRIMARY KEY (id_interlocuteur )
@@ -75,16 +77,14 @@ CREATE TABLE INTERLOCUTEUR(
 # Table: PARTICULIER
 #------------------------------------------------------------
 
-DROP TABLE PARTICULIER;
-
 CREATE TABLE PARTICULIER(
-        num_individu         int (10) Auto_increment  NOT NULL ,
-        adresse_individu     Varchar (50) NOT NULL ,
-        cp_individu          Varchar (10) NOT NULL ,
-        ville_individu       Varchar (20) NOT NULL ,
+        num_individu         int (11) Auto_increment  NOT NULL ,
+        adresse_individu     Varchar (25) NOT NULL ,
+        cp_individu          Varchar (25) NOT NULL ,
+        ville_individu       Varchar (25) NOT NULL ,
         distance_siege       Float NOT NULL ,
-        plan_accees          Varchar (100) NOT NULL ,
-        infos_supplementaire Varchar (100) NOT NULL ,
+        plan_accees          Varchar (25) NOT NULL ,
+        infos_supplementaire Varchar (25) NOT NULL ,
         id_interlocuteur     Int NOT NULL ,
         PRIMARY KEY (num_individu )
 )ENGINE=InnoDB;
@@ -94,20 +94,18 @@ CREATE TABLE PARTICULIER(
 # Table: RDV
 #------------------------------------------------------------
 
-DROP TABLE RDV;
-
 CREATE TABLE RDV(
-        id_rdv                int (10) Auto_increment  NOT NULL ,
+        id_rdv                int (11) Auto_increment  NOT NULL ,
         date_rdv              Date NOT NULL ,
-        heure_debut           Time NOT NULL ,
-        heure_fin             Time NOT NULL ,
+        heure_debut           Date NOT NULL ,
+        heure_fin             Date NOT NULL ,
         adresseDerogatoire    Varchar (50) ,
-        villeDerogatoire      Varchar (20) ,
+        villeDerogatoire      Varchar (30) ,
         codeEntreeDerogatoire Int ,
-        infoDerogatoire       Varchar (100) ,
+        infoDerogatoire       Varchar (500) ,
         id_interlocuteur      Int NOT NULL ,
         id_type_rdv           Int NOT NULL ,
-        idPlanning            Varchar (10) ,
+        idPlanning            Varchar (25) ,
         PRIMARY KEY (id_rdv )
 )ENGINE=InnoDB;
 
@@ -116,39 +114,17 @@ CREATE TABLE RDV(
 # Table: CONGES
 #------------------------------------------------------------
 
-DROP TABLE CONGES;
-
 CREATE TABLE CONGES(
         numConge       Int NOT NULL ,
         dateDebutConge Datetime ,
         dateFinConge   Datetime ,
-        id_salarie     Int NOT NULL ,
         PRIMARY KEY (numConge )
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: UTILISATEUR
-#------------------------------------------------------------
-
-DROP TABLE UTILISATEUR;
-
-CREATE TABLE UTILISATEUR(
-        numUtilisateur      Int NOT NULL ,
-        loginUtilisateur    Varchar (50) ,
-        passwordUtilisateur Varchar (50) ,
-        dateDernierLogin    Datetime ,
-        nbTentatives        Int ,
-        codeTypeUtilisateur Int NOT NULL ,
-        PRIMARY KEY (numUtilisateur )
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
 # Table: TYPE_UTILISATEUR
 #------------------------------------------------------------
-
-DROP TABLE TYPE_UTILISATEUR;
 
 CREATE TABLE TYPE_UTILISATEUR(
         codeTypeUtilisateur    Int NOT NULL ,
@@ -161,13 +137,11 @@ CREATE TABLE TYPE_UTILISATEUR(
 # Table: MAIL
 #------------------------------------------------------------
 
-DROP TABLE MAIL;
-
 CREATE TABLE MAIL(
         numMail          Int NOT NULL ,
         contenuMail      Varchar (10000) ,
         objetMail        Varchar (50) ,
-        id_salarie       Int NOT NULL ,
+        numUtilisateur   Int NOT NULL ,
         id_interlocuteur Int NOT NULL ,
         PRIMARY KEY (numMail )
 )ENGINE=InnoDB;
@@ -177,11 +151,9 @@ CREATE TABLE MAIL(
 # Table: PLANNING
 #------------------------------------------------------------
 
-DROP TABLE PLANNING;
-
 CREATE TABLE PLANNING(
-        idPlanning Varchar (10) NOT NULL ,
-        id_salarie Int NOT NULL ,
+        idPlanning     Varchar (25) NOT NULL ,
+        numUtilisateur Int NOT NULL ,
         PRIMARY KEY (idPlanning )
 )ENGINE=InnoDB;
 
@@ -189,8 +161,6 @@ CREATE TABLE PLANNING(
 #------------------------------------------------------------
 # Table: TYPE_STRUCTURE
 #------------------------------------------------------------
-
-DROP TABLE TYPE_STRUCTURE;
 
 CREATE TABLE TYPE_STRUCTURE(
         codeTypeStructure    Int NOT NULL ,
@@ -203,12 +173,10 @@ CREATE TABLE TYPE_STRUCTURE(
 # Table: PORTEFEUILLE
 #------------------------------------------------------------
 
-DROP TABLE PORTEFEUILLE;
-
 CREATE TABLE PORTEFEUILLE(
         idPortefeuille      Int NOT NULL ,
         libellePortefeuille Varchar (30) ,
-        id_salarie          Int NOT NULL ,
+        numUtilisateur      Int NOT NULL ,
         PRIMARY KEY (idPortefeuille )
 )ENGINE=InnoDB;
 
@@ -216,8 +184,6 @@ CREATE TABLE PORTEFEUILLE(
 #------------------------------------------------------------
 # Table: APPARTENIR
 #------------------------------------------------------------
-
-DROP TABLE APPARTENIR;
 
 CREATE TABLE APPARTENIR(
         id_interlocuteur Int NOT NULL ,
@@ -227,13 +193,43 @@ CREATE TABLE APPARTENIR(
 
 
 #------------------------------------------------------------
-# Table: SUIVRE
+# Table: POSER
 #------------------------------------------------------------
 
-DROP TABLE SUIVRE;
+CREATE TABLE POSER(
+        numUtilisateur Int NOT NULL ,
+        numConge       Int NOT NULL ,
+        PRIMARY KEY (numUtilisateur ,numConge )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: SUIVRE
+#------------------------------------------------------------
 
 CREATE TABLE SUIVRE(
         id_rdv     Int NOT NULL ,
         id_rdv_RDV Int NOT NULL ,
         PRIMARY KEY (id_rdv ,id_rdv_RDV )
 )ENGINE=InnoDB;
+
+ALTER TABLE UTILISATEUR ADD CONSTRAINT FK_UTILISATEUR_codeTypeUtilisateur FOREIGN KEY (codeTypeUtilisateur) REFERENCES TYPE_UTILISATEUR(codeTypeUtilisateur);
+ALTER TABLE UTILISATEUR ADD CONSTRAINT FK_UTILISATEUR_idPlanning FOREIGN KEY (idPlanning) REFERENCES PLANNING(idPlanning);
+ALTER TABLE UTILISATEUR ADD CONSTRAINT FK_UTILISATEUR_idPortefeuille FOREIGN KEY (idPortefeuille) REFERENCES PORTEFEUILLE(idPortefeuille);
+ALTER TABLE STRUCTURE ADD CONSTRAINT FK_STRUCTURE_codeTypeStructure FOREIGN KEY (codeTypeStructure) REFERENCES TYPE_STRUCTURE(codeTypeStructure);
+ALTER TABLE INTERLOCUTEUR ADD CONSTRAINT FK_INTERLOCUTEUR_num_individu FOREIGN KEY (num_individu) REFERENCES PARTICULIER(num_individu);
+ALTER TABLE INTERLOCUTEUR ADD CONSTRAINT FK_INTERLOCUTEUR_idPortefeuille FOREIGN KEY (idPortefeuille) REFERENCES PORTEFEUILLE(idPortefeuille);
+ALTER TABLE PARTICULIER ADD CONSTRAINT FK_PARTICULIER_id_interlocuteur FOREIGN KEY (id_interlocuteur) REFERENCES INTERLOCUTEUR(id_interlocuteur);
+ALTER TABLE RDV ADD CONSTRAINT FK_RDV_id_interlocuteur FOREIGN KEY (id_interlocuteur) REFERENCES INTERLOCUTEUR(id_interlocuteur);
+ALTER TABLE RDV ADD CONSTRAINT FK_RDV_id_type_rdv FOREIGN KEY (id_type_rdv) REFERENCES TYPERDV(id_type_rdv);
+ALTER TABLE RDV ADD CONSTRAINT FK_RDV_idPlanning FOREIGN KEY (idPlanning) REFERENCES PLANNING(idPlanning);
+ALTER TABLE MAIL ADD CONSTRAINT FK_MAIL_numUtilisateur FOREIGN KEY (numUtilisateur) REFERENCES UTILISATEUR(numUtilisateur);
+ALTER TABLE MAIL ADD CONSTRAINT FK_MAIL_id_interlocuteur FOREIGN KEY (id_interlocuteur) REFERENCES INTERLOCUTEUR(id_interlocuteur);
+ALTER TABLE PLANNING ADD CONSTRAINT FK_PLANNING_numUtilisateur FOREIGN KEY (numUtilisateur) REFERENCES UTILISATEUR(numUtilisateur);
+ALTER TABLE PORTEFEUILLE ADD CONSTRAINT FK_PORTEFEUILLE_numUtilisateur FOREIGN KEY (numUtilisateur) REFERENCES UTILISATEUR(numUtilisateur);
+ALTER TABLE APPARTENIR ADD CONSTRAINT FK_APPARTENIR_id_interlocuteur FOREIGN KEY (id_interlocuteur) REFERENCES INTERLOCUTEUR(id_interlocuteur);
+ALTER TABLE APPARTENIR ADD CONSTRAINT FK_APPARTENIR_num_structure FOREIGN KEY (num_structure) REFERENCES STRUCTURE(num_structure);
+ALTER TABLE POSER ADD CONSTRAINT FK_POSER_numUtilisateur FOREIGN KEY (numUtilisateur) REFERENCES UTILISATEUR(numUtilisateur);
+ALTER TABLE POSER ADD CONSTRAINT FK_POSER_numConge FOREIGN KEY (numConge) REFERENCES CONGES(numConge);
+ALTER TABLE SUIVRE ADD CONSTRAINT FK_SUIVRE_id_rdv FOREIGN KEY (id_rdv) REFERENCES RDV(id_rdv);
+ALTER TABLE SUIVRE ADD CONSTRAINT FK_SUIVRE_id_rdv_RDV FOREIGN KEY (id_rdv_RDV) REFERENCES RDV(id_rdv);
