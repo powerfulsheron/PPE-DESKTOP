16a0fd747537fa05164cbfebe59ef08107db89e3
Canavaggio Lorenzo <lorenzo.canavaggio@laposte.net>
2017-03-12 01:07:33 +0100

    Méthodes

---
+++ Méthodes/Liste Objet de la Base.cs
@@ -0,0 +1,67 @@
+﻿using System; 
+using System.Collections; 
+using System.Collections.Generic; 
+using NHibernate; 
+using NHibernate.Cfg; 
+using log4net;
+
+namespace PamNHibernateDemos 
+{
+    class Program
+    {
+        private static ISessionFactory sessionFactory = null; 
+        
+ 
+ 
+    // programme principal 
+    static void Main(string[] args) 
+    {
+
+      // initialisation factory NHibernate 
+      sessionFactory = new Configuration().Configure().BuildSessionFactory(); 
+      try 
+      { 
+        // affichage contenu de la base 
+        Console.WriteLine("Affichage base -------------------------------------"); 
+        ShowDataBase1(); 
+      } 
+      catch (Exception ex) 
+      { 
+        // on affiche l'exception  
+        Console.WriteLine(string.Format("L'erreur suivante s'est produite : [{0}]", ex.ToString())); 
+      } 
+      finally 
+      { 
+        if (sessionFactory != null) 
+        { 
+          sessionFactory.Close(); 
+        } 
+      } 
+      // attente clavier 
+      Console.ReadLine(); 
+    } 
+ 
+    // test1 
+    static void ShowDataBase1() 
+    { 
+      // ouverture session  
+      using (ISession session = sessionFactory.OpenSession()) 
+      { 
+        // début transaction 
+        using (ITransaction transaction = session.BeginTransaction()) 
+        { 
+          // on récupère la liste des employés 
+            IList<Utilisateur> utilisateurs = session.CreateQuery(@"select e from Utilisateur e order by e.nomUtilisateur asc").List<Utilisateur>(); 
+          // on l'affiche 
+          Console.WriteLine("--------------- liste des employés");
+          foreach (Utilisateur e in utilisateurs) 
+          { 
+            Console.WriteLine(e); 
+          } 
+          // commit transaction 
+          transaction.Commit(); 
+        } 
+      } 
+    } 
+        }
+    }
---
+++ Méthodes/Login.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Linq;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PamNHibernateDemos
+{
+    class Program
+    {
+        private static ISessionFactory sessionFactory = null;
+
+
+
+        // programme principal 
+        static void Main(string[] args)
+        {
+            string passwordInput;
+            Utilisateur leLoger = new Utilisateur();
+            // initialisation factory NHibernate 
+            sessionFactory = new Configuration().Configure().BuildSessionFactory();
+            try
+            {
+
+                Console.WriteLine("Utilisateur:");
+                try
+                {
+                    leLoger = ShowDataBase1(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                Console.WriteLine("Password:");
+                passwordInput = Console.ReadLine();
+
+                byte[] encodedPassword = new UTF8Encoding().GetBytes(passwordInput);
+
+                // need MD5 to calculate the hash
+                byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5")).ComputeHash(encodedPassword);
+
+                // string representation (similar to UNIX format)
+                string encoded = BitConverter.ToString(hash)
+                    // without dashes
+                   .Replace("-", string.Empty)
+                    // make lowercase
+                   .ToLower();
+
+                if (encoded == leLoger.passwordUtilisateur) { Console.WriteLine("gg fdp"); }
+                else { Console.WriteLine("Au GOULAG fdp!"); }
+
+                Console.WriteLine(leLoger);
+                Console.ReadKey();
+
+
+            }
+            catch (Exception ex)
+            {
+                // on affiche l'exception  
+                Console.WriteLine(string.Format("L'erreur suivante s'est produite : [{0}]", ex.ToString()));
+            }
+            finally
+            {
+                if (sessionFactory != null)
+                {
+                    sessionFactory.Close();
+                }
+            }
+            // attente clavier 
+            Console.ReadLine();
+        }
+
+        // test1 
+        static Utilisateur ShowDataBase1(string stringloginUtilisateur)
+        {
+            // ouverture session  
+            ISession session = sessionFactory.OpenSession();
+
+
+            // on récupère l'employé
+            return session.Query<Utilisateur>().SingleOrDefault(w => w.loginUtilisateur == stringloginUtilisateur);
+
+
+
+
+
+        }
+    }
+}
