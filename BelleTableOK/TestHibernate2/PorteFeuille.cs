d3a4bc953f1dba234d3c5e3f8734aca05a21dd4f
Canavaggio Lorenzo <lorenzo.canavaggio@laposte.net>
2017-03-12 01:02:30 +0100

    PROJET DE BASE A UTILISER
    PROJET DE BASE A UTILISER

---
+++ BelleTableOK/TestHibernate2/PorteFeuille.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PamNHibernateDemos
+{
+    public class PorteFeuille
+    {
+        public virtual int idPortefeuille { get; set; }
+        public virtual string libellePortefeuille { get; set; }
+        public virtual IList<Interlocuteur> lesInterlocuteurs { get; set; }
+        public virtual Utilisateur utilisateur { get; set; }
+       
+        public PorteFeuille()
+        {
+
+        }
+
+        // ToString 
+        public override string ToString()
+        {
+            return string.Format("[{0}|{1}]", libellePortefeuille, lesInterlocuteurs);
+        }
+      
+    }
+}
---
+++ BelleTableOK/TestHibernate2/TypeUtilisateur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PamNHibernateDemos
+
+{
+    public class TypeUtilisateur
+    {
+        public virtual int codeTypeUtilisateur { get; set; }
+        public virtual string libelleTypeUtilisateur { get; set; }
+
+        public TypeUtilisateur()
+        {
+
+        }
+
+        // ToString 
+        public override string ToString()
+        {
+            return string.Format("[{0}]", libelleTypeUtilisateur);
+        }
+     
+    }
+
+}
