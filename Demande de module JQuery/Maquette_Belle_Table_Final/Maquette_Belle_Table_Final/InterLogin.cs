9cb9d05d7291077cb3f3846d2e10d842b02e615c
Canavaggio Lorenzo <lorenzo.canavaggio@laposte.net>
2017-03-24 13:53:58 +0100

    Demande de module pour le planning jQuery
    Demande de module pour le planning jQuery

---
+++ Demande de module JQuery/Maquette_Belle_Table_Final/Maquette_Belle_Table_Final/InterLogin.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Linq;
+using System.Security.Cryptography;
+using System.Net.Mail;
+
+
+namespace Maquette_Belle_Table_Final
+{
+    public partial class InterLogin : Form
+    {
+        private static ISessionFactory sessionFactory = null;
+        public InterLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void labelFermeture_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void linkLabelMDP_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            panelMDPO.Visible = true;
+        }
+
+        private void buttonConnexion_Click(object sender, EventArgs e)
+        {
+            // connection à la bdd avec hibernate
+            sessionFactory = new Configuration().Configure().BuildSessionFactory();
+            ISession session = sessionFactory.OpenSession();
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                // on charge l'utilisateur avec le login spécifié dans la textbox
+                Utilisateur utilisateur = session.Query<Utilisateur>().SingleOrDefault(w => w.loginUtilisateur == textBoxId.Text);
+                // si null, erreur
+                if (utilisateur == null)
+                {
+                    MessageBox.Show("Utilisateur introuvable", "Erreur");
+                }
+                //sinon on verifie que le password match et si loggé on redirige l'utilisateur en fonction de son type.
+                else if (MD5Hash(textBoxMDP.Text) == utilisateur.passwordUtilisateur)
+                {
+                    // on réinitialise le nombre de tentatives à 0
+                    utilisateur.nbTentatives = 0;
+                    session.Update(utilisateur);
+                    transaction.Commit();
+                    if (utilisateur.typeUtilisateur.codeTypeUtilisateur == 1)
+                    {                     
+                        new InterAd().Show();
+                        InterAd interAd = new InterAd();
+                        interAd.utilisateur = utilisateur;
+                        interAd.Show();
+                    }
+
+                    else if (utilisateur.typeUtilisateur.codeTypeUtilisateur == 2)
+                    {    
+                        new InterGes().Show();
+                        InterGes interGes = new InterGes();
+                        interGes.utilisateur = utilisateur;
+                        interGes.Show();
+                    }
+
+                    else if (utilisateur.typeUtilisateur.codeTypeUtilisateur == 3)
+                    {
+                        new InterUti().Show();
+                        InterUti interUti = new InterUti();
+                        interUti.utilisateur = utilisateur;
+                        interUti.Show();
+                    }
+
+                }
+                else // erreur de mdp
+                {
+                    MessageBox.Show("Il semble que le mot de passe ne soit pas correct", "Erreur");
+                    // on gère le nombre de tentatives >6
+                    utilisateur.nbTentatives = utilisateur.nbTentatives + 1;
+                    if (utilisateur.nbTentatives >= 6)
+                    {
+                        utilisateur.nbTentatives = 0;
+                        try
+                        {
+                            MailMessage mail = new MailMessage();
+                            mail.Subject = "[GEPEV] Tentatives de connection excessives";
+                            mail.Body = "l'utilisateur : " + utilisateur.nomUtilisateur + " " + utilisateur.prenomUtilisateur + " à fait 6 échecs de connections à l'application GEPEV.";
+                            mail.From = new MailAddress("bot@belletable.com");
+                            mail.To.Add("admin@belletable.com");
+
+                            SmtpClient client = new SmtpClient();
+                            client.Host = "localhost";
+                            client.Send(mail);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        panelMDPO.Visible = true;
+                    }
+
+                    session.Update(utilisateur);
+                    transaction.Commit();
+                }
+
+
+            }
+
+        }
+
+        private void buttonAnnu_Click(object sender, EventArgs e)
+        {
+            buttonAnnu.DialogResult = DialogResult.Cancel;
+            panelMDPO.Visible = false;
+        }
+
+        private void buttonVal_Click(object sender, EventArgs e)
+        {
+            buttonVal.DialogResult = DialogResult.OK;
+            if (buttonVal.DialogResult == DialogResult.OK)
+            {
+                MessageBox.Show("Votre nouveau mot de passe a été envoyer.");
+            }
+        }
+
+        private void buttonT1_Click(object sender, EventArgs e)
+        {
+            new InterAd().Show();
+        }
+
+        private void buttonT2_Click(object sender, EventArgs e)
+        {
+            new InterGes().Show();
+        }
+
+        private void buttonT3_Click(object sender, EventArgs e)
+        {
+            new InterUti().Show();
+        }
+
+        public static string MD5Hash(string input)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(input));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            return hash.ToString();
+
+        }
+    }
+}
+
---
+++ Demande de module JQuery/Maquette_Belle_Table_Final/Maquette_Belle_Table_Final/Popup AddModUser.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maquette_Belle_Table
+{
+    public partial class Popup_AddModUser : Form
+    {
+        public Popup_AddModUser()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Popup_AddModUser.ActiveForm.Close();
+        }
+
+        private void radioButtonCom_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonCom.Checked)
+            {
+                panelCom.Visible = true;
+                panelAdmin.Visible = false;
+                panelGest.Visible = false;
+            }
+        }
+
+        private void radioButtonAdm_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonAdm.Checked)
+            {
+                panelAdmin.Visible = true;
+                panelGest.Visible = false;
+                panelCom.Visible = false;
+
+            }
+        }
+
+        private void radioButtonUti_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonUti.Checked)
+            {
+                panelGest.Visible = true;
+                panelCom.Visible = false;
+                panelAdmin.Visible = false;
+
+
+            }
+        }
+        //Bouton Annuler sur Profil Admin
+        private void buttonAnuAdm_Click(object sender, EventArgs e)
+        {
+            buttonAnuAdm.DialogResult = DialogResult.Cancel;
+        }
+
+        //Bouton Valider sur Profil Admin
+        private void buttonValAdm_Click(object sender, EventArgs e)
+        {
+            buttonValAdm.DialogResult = DialogResult.OK;
+        }
+
+        //Bouton Annuler sur Profil Gestionnaire
+        private void buttonAnuGes_Click(object sender, EventArgs e)
+        {
+            buttonAnuGes.DialogResult = DialogResult.Cancel;
+        }
+
+        //Bouton Valider sur Profil Gestionnaire
+        private void buttonValGes_Click(object sender, EventArgs e)
+        {
+            buttonValGes.DialogResult = DialogResult.OK;
+        }
+
+        //Bouton Annuler sur Profil Commercial
+        private void buttonAnuCom_Click(object sender, EventArgs e)
+        {
+            buttonAnuCom.DialogResult = DialogResult.Cancel;
+        }
+
+        //Bouton Valider sur Profil Commercial
+        private void buttonValCom_Click(object sender, EventArgs e)
+        {
+            buttonValCom.DialogResult = DialogResult.OK;
+        }
+    }
+
+}
