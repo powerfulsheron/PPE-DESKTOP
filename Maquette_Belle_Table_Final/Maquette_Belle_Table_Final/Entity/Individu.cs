65059a6e51c99213cfff1de9a20dfd5f696cb020
RaphaelDG <Raphae.dulong@gmail.com>
2017-04-10 14:37:38 +0200

    Revert "Maj ok"
    This reverts commit db4bc94aecf992182344edcf2c6801a15c560f2a.

--- Maquette_Belle_Table_Final/Maquette_Belle_Table_Final/Entity/Individu.cs
+++ Maquette_Belle_Table_Final/Maquette_Belle_Table_Final/Entity/Individu.cs
@@ -10,6 +10,7 @@ namespace Maquette_Belle_Table_Final
         public virtual string adresseIndividu { get; set; }
         public virtual string cpIndividu { get; set; }
         public virtual string villeIndividu { get; set; }
+        public virtual string planAcces { get; set; }
         public virtual string infosSupplementaire { get; set; }
         public virtual Interlocuteur interlocuteur { get; set; }
 
@@ -21,7 +22,7 @@ namespace Maquette_Belle_Table_Final
         // ToString 
         public override string ToString()
         {
-            return string.Format("[{0}|{1}|{2}|{3}|{4}]", distanceSiege, adresseIndividu, cpIndividu, villeIndividu, infosSupplementaire);
+            return string.Format("[{0}|{1}|{2}|{3}|{4}|{5}]", distanceSiege, adresseIndividu, cpIndividu, villeIndividu, planAcces, infosSupplementaire);
         }
         
     }
