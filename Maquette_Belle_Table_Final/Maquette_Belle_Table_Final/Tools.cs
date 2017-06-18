cddbdb05395a4fb829c0e9e71cfa66fe9e3d4922
RaphaelDG <Raphael.dulong@gmail.com>
2017-06-18 22:40:30 +0200

    Update Tools.cs

--- Maquette_Belle_Table_Final/Maquette_Belle_Table_Final/Tools.cs
+++ Maquette_Belle_Table_Final/Maquette_Belle_Table_Final/Tools.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using iTextSharp;
 using iTextSharp.text.pdf;
 using iTextSharp.text;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 //Classe référençant divers méthodes "outils"
 
@@ -81,8 +84,8 @@ namespace Maquette_Belle_Table_Final
             nouveauDocument.Close();
         }
 
-           // public void ConnectMySQL()
-            //{SCRIPT POUR SE CONNECTER A MYSQL SUR WAMP EN LOCAL SANS NHIBERNATE
+            public void ConnectMySQL()
+            {//SCRIPT POUR SE CONNECTER A MYSQL SUR WAMP EN LOCAL SANS NHIBERNATE
 
             // 1. ajouter aux références "SQL.Data" 
 
@@ -90,21 +93,62 @@ namespace Maquette_Belle_Table_Final
 
             //using MySql.Data.MySqlClient;
              //3. et enfin :
-             //   MySqlConnection Connect = new MySqlConnection("Server = localhost; Port = 3306; Database = gepev; Uid = root; Pwd =;"); 
+                MySqlConnection Connect = new MySqlConnection("Server = localhost; Port = 3306; Database = gepev; Uid = root; Pwd =;"); 
                 //a modifier si différent pour sois-même
-                  //      try
-                  //      {
-                  //          Connect.Open();
+                        try
+                        {
+                            Connect.Open();
                             //MessageBox.Show("Connecté à la base", "tu es connecté");  
                 
-                    //       }
-                    //    catch(MySqlException ex)
-                     //   {
-                        //        MessageBox.Show(ex.ToString());
-                            //    }
+                           }
+                        catch(MySqlException ex)
+                        {
+                                MessageBox.Show(ex.ToString());
+                                }
 				
                                  //Connect.Close();
-               // }
+                }
         //Graphiques: http://plasserre.developpez.com/cours/chart/ et voir le Windows Form GraphDemo
+        public void HelloChart()
+        {
+            //Le chart et les deux DateTimePicker doivent être déposé dans le formulaire via la boite à outil
+            //il n'y aura donc pas besoin de les déclarer comme sur les 3 lignes qui suivent:
+            Chart chart1 = new Chart();
+            DateTimePicker dateTimePicker1 = new DateTimePicker();
+            DateTimePicker dateTimePicker2 = new DateTimePicker();
+
+            //Dans un graphique Chart il y a une ou plusieur Série de donnée, ici on en veut une seule
+            chart1.Series.Add("Series1");
+
+            //On détermine la date minimum et maximum qui apparaitera sur l'axe Y du graphique
+            //AxisY.Minimum ne reçoit que des type 'doubles', dateTimePicker2.Value.Date.ToOADate(); est un double
+            chart1.ChartAreas["ChartArea1"].AxisY.Minimum = dateTimePicker2.Value.Date.ToOADate();
+            chart1.ChartAreas["ChartArea1"].AxisY.Maximum = dateTimePicker1.Value.Date.ToOADate();
+
+            // On fait une requete select toute simple
+            MySqlConnection conDataBase = new MySqlConnection("Server = localhost; Port = 3306; Database = gepev; Uid = root; Pwd =;");
+            MySqlCommand cmdDataBase = new MySqlCommand(" select * from utilisateur ;", conDataBase);
+            //Un reader qui nous servira à lire la requête
+            MySqlDataReader myReader;
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    //A chaque ligne trouvée on ajoute à l'axe X le 'prenomUtilisateur' auquel on associe sur l'axe Y la date 'dateDernierLogin'
+                    // les données proviennent de la BDD
+                    // On remarque GetString ou GetDateTime en fonction du type de donnée
+                    chart1.Series["Series1"].Points.AddXY(myReader.GetString("prenomUtilisateur"), myReader.GetDateTime("dateDernierLogin"));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
     }
 }
